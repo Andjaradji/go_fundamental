f16f3226dcfcdbfc624abd68fca824feb86d3d19
Andjaradji <andjaradji.rooseno@klikacc.com>
2021-05-08 00:09:37 +0700

    conditional if temp variable example

--- conditional_if/main.go
+++ conditional_if/main.go
@@ -3,9 +3,23 @@ package main
 import "fmt"
 
 func main(){
-	point := 8
+	// point := 8
 
-	if point <= 5 {
+	// if point <= 5 {
+	// 	fmt.Println("Maaf Anda Tidak Lulus")
+	// } else if point <= 6 {
+	// 	fmt.Println("Anda lulus pas-pasan")
+	// } else if point <= 7 {
+	// 	fmt.Println("Anda Lulus Cukup Baik")
+	// } else if point <= 8 {
+	// 	fmt.Println("Anda Lulus dengan Sangat Baik")
+	// } else {
+	// 	fmt.Println("Anda Luar Biasa")
+	// }
+
+	// using point as temporary variable
+
+	if point :=3; point <=5 {
 		fmt.Println("Maaf Anda Tidak Lulus")
 	} else if point <= 6 {
 		fmt.Println("Anda lulus pas-pasan")
@@ -16,4 +30,6 @@ func main(){
 	} else {
 		fmt.Println("Anda Luar Biasa")
 	}
+
+
 }
