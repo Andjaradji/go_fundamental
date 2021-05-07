68346a9f2c3d6258f77133ffa5cd6d2ee44f6601
Andjaradji <andjaradji.rooseno@klikacc.com>
2021-05-07 15:10:21 +0700

    example for variable initialization and declaration

---
+++ variable/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "Andjaradji"
+
+	var lastName = "Rooseno"
+
+	fullName := "Andjaradji Rooseno"
+
+	var nickName string
+
+	nickName = "Dimas"
+	
+
+	fmt.Println(firstName, lastName)
+	fmt.Println(fullName)
+	fmt.Println(nickName)
+}
